266b220ed40e2c6ee5eb9d7188f09edc5906f47a
Ariel Fermin Belossi <ariel.belossi@y.uno>
2023-08-31 23:16:04 -0300

    Notebooks 001 to 005 done

--- datagen.py
+++ datagen.py
@@ -1,11 +1,11 @@
 import numpy as np
-import tensorflow.keras as keras
-
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from dataaug import SmilesEnumerator 
 
 smiles_dict = {'#': 1, '(': 2, ')': 3, '+': 4, '-': 5, '/': 6, '1': 7, '2': 8, '3': 9, '4': 10, '5': 11, '6': 12, '7': 13, '8': 14, '=': 15, 'C': 16, 'F': 17, 'H': 18, 'I': 19, 'N': 20, 'O': 21, 'P': 22, 'S': 23, '[': 24, '\\': 25, ']': 26, '_': 27, 'c': 28, 'Cl': 29, 'Br': 30, 'n': 31, 'o': 32, 's': 33, '@': 34, '.': 35, 'a': 36, 'B': 37, 'e': 38, 'i': 39, '9': 40, '10': 41, '11': 42}
 
 
-# Pasa 
 def smiles_to_seq(smiles, seq_length, char_dict=smiles_dict):
     """ Tokenize characters in smiles to integers
     """
@@ -29,40 +29,73 @@ def smiles_to_seq(smiles, seq_length, char_dict=smiles_dict):
             print(smiles[i:i + 1], i)
             raise ValueError('character not found in dict')
     for i in range(seq_length - len(seq)):
-      # Padding with '_'
-      seq.append(0)
+        # Padding with '_'
+        seq.append(0)
     return seq
 
-
-from dataaug import SmilesEnumerator
-
-class DataGenerator(keras.utils.Sequence):
+class DataGenerator(tf.keras.utils.Sequence):
     def __init__(self, X, y, seq_length, batch_size=128, data_augmentation=True, shuffle=True):
-        # Agregar aca todas las propiedades necesarias para resolver el problema
-        # No olvidar la aumentación de datos
-        self.on_epoch_end()
-        
-
+        self.X = X
+        self.y = y
+        self.seq_length = seq_length
+        self.batch_size = batch_size
+        self.data_augmentation = data_augmentation
+        self.shuffle = shuffle
+        self.sme = SmilesEnumerator()
+        self.indexes = np.arange(len(self.X))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
 
     def __len__(self):
-        'Denotes the number of batches per epoch'
         return int(np.ceil(len(self.X) / self.batch_size))
 
     def __getitem__(self, index):
-        'Generate one batch of data'
-        # Generate indexes of the batch
-        indexes = # Implementar
+        batch_indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+        batch_X = [self.X[i] for i in batch_indexes]
+        batch_y = [self.y[i] for i in batch_indexes]
 
-        # Generate data
         if self.data_augmentation:
-            # Implementar
+            augmented_X = [self.augment_smiles(smiles) for smiles in batch_X]
+            padded_X = self.pad_sequences(augmented_X)
         else:
-            # Implementar
-        y = # Implementar
-        return X, y
+            padded_X = self.pad_sequences(batch_X)
+
+        padded_X = [smiles_to_seq(x, seq_length=self.seq_length) for x in padded_X]
+
+        return np.array(padded_X), np.array(batch_y)
 
     def on_epoch_end(self):
-        'Updates indexes after each epoch'
-        self.indexes = np.arange(len(self.X))
-        if self.shuffle == True:
-            # Implementar
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def augment_smiles(self, smiles):
+        augmented_smiles = self.sme.randomize_smiles(smiles)
+        return augmented_smiles
+
+    def pad_sequences(self, sequences):
+        padded_sequences = []
+        for seq in sequences:
+            if len(seq) < self.seq_length:
+                padded_seq = seq + ' ' * (self.seq_length - len(seq))
+            else:
+                padded_seq = seq[:self.seq_length]
+            padded_sequences.append(padded_seq)
+        return padded_sequences
+
+    def smiles_to_seq(self, smiles):
+        smiles_len = len(smiles)
+        seq = []
+        keys = smiles_dict.keys()
+        i = 0
+        while i < smiles_len:
+            if smiles[i:i + 2] in keys:
+                seq.append(smiles_dict[smiles[i:i + 2]])
+                i = i + 2
+            elif smiles[i:i + 1] in keys:
+                seq.append(smiles_dict[smiles[i:i + 1]])
+                i = i + 1
+            else:
+                raise ValueError('character not found in dict')
+        for i in range(self.seq_length - len(seq)):
+            seq.append(0)
+        return seq
